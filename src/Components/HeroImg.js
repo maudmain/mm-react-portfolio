59e0326e95f7feef70caa33ed704d2c9b848e034
maudmain <117314424+maudmain@users.noreply.github.com>
2023-03-12 20:32:19 +0000

    project hero added an bg color fixed (root removed where necessary)

---
+++ src/Components/HeroImg.js
@@ -0,0 +1,18 @@
+import "./heroImg.css";
+
+import React from 'react'
+
+const HeroImg = () => {
+  return (
+    <div className="hero-img">
+      <div className="heading">
+        <h1>Projects</h1>
+        <p>Check out my latest projects.</p>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default HeroImg
